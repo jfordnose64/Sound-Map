6ae968e54c9f0b571db25cb8a135e6255d4b68a0
jackson ford <jfordnose64@gmail.com>
2020-01-29 13:21:40 -0500

    working on Sound-Map

--- Components/Record.tsx
+++ Components/Record.tsx
@@ -4,13 +4,13 @@ import { View, Text, StyleSheet, AsyncStorage } from 'react-native'
 import { Button } from 'react-native-elements'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import * as Permissions from 'expo-permissions'
-import { Audio } from 'expo-av'
 import Recording from './Recording'
 
 class Record extends React.Component {
   state = {
     color: 'red',
-    icon: 'microphone'
+    icon: 'microphone',
+    iconColor: 'white'
   }
 
   checkMultiPermissions = async () => {
@@ -24,21 +24,50 @@ class Record extends React.Component {
     }
   }
 
+  record = () => {
+    if (this.state.color === 'red') {
+      this.setState({
+        color: 'white',
+        icon: 'square',
+        iconColor: 'red'
+      })
+      console.log('Recording')
+    } else if (this.state.color === 'white') {
+      this.setState({
+        color: 'red',
+        icon: 'microphone',
+        iconColor: 'white'
+      })
+      console.log('Stopped Recording')
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Recording />
         <Button title="request" onPress={() => this.checkMultiPermissions()} />
-        <Button
-          buttonStyle={{
-            backgroundColor: this.state.color,
-            borderRadius: 50,
-            margin: 0,
-            padding: 0
-          }}
-          onPress={() => this.yes}
-          icon={<Icon name={this.state.icon} size={15} color="white" />}
-        />
+        <View style={styles.button}>
+          <Button
+            containerStyle={{}}
+            buttonStyle={{
+              backgroundColor: this.state.color,
+              borderRadius: 50,
+              margin: 0,
+              padding: 0,
+              width: 50,
+              height: 50
+            }}
+            onPress={() => this.record()}
+            icon={
+              <Icon
+                name={this.state.icon}
+                size={30}
+                color={this.state.iconColor}
+              />
+            }
+          />
+        </View>
         <Text style={styles.header}>Record</Text>
       </View>
     )
@@ -56,6 +85,12 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 25,
     margin: 10
+  },
+  button: {
+    width: '100%',
+    height: '30%',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
 
--- Components/Recording.tsx
+++ Components/Recording.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { View, Text, StyleSheet, AsyncStorage } from 'react-native'
-import { Audio } from 'expo-av'
+import AudioRecorderPlayer from 'react-native-audio-recorder-player'
 import { Button } from 'react-native-elements'
 
 class Recording extends React.Component {
@@ -9,31 +9,81 @@ class Recording extends React.Component {
     canRecord: false,
     isRecording: false,
     isDoneRecording: false,
-    durationMillis: 0
+    durationMillis: 0,
+    recordSecs: 0
   }
 
-  onPressRecord = async () => {
-    const record = new Audio.Recording()
-    try {
-      await record.prepareToRecordAsync(
-        Audio.RECORDING_OPTIONS_PRESET_LOW_QUALITY
-      )
-      await record.startAsync()
-      alert(`onPressRecord recording!`)
-    } catch (error) {
-      alert(`onPressRecord error: ${error}`)
-    }
+  audioRecorderPlayer = new AudioRecorderPlayer()
+
+  onStartRecord = async () => {
+    const audioRecorderPlayer = new AudioRecorderPlayer()
+    const result = await this.audioRecorderPlayer.startRecorder()
+    this.audioRecorderPlayer.addRecordBackListener(e => {
+      this.setState({
+        recordSecs: e.current_position,
+        recordTime: this.audioRecorderPlayer.mmssss(
+          Math.floor(e.current_position)
+        )
+      })
+      return
+    })
+    console.log(result)
   }
 
-  startRecording = () => {}
+  onStopRecord = async () => {
+    const result = await this.audioRecorderPlayer.stopRecorder()
+    this.audioRecorderPlayer.removeRecordBackListener()
+    this.setState({
+      recordSecs: 0
+    })
+    console.log(result)
+    console.log(this.state.recordSecs)
+  }
 
-  stopRecording = () => {}
+  onStartPlay = async () => {
+    console.log('onStartPlay')
+    const msg = await this.audioRecorderPlayer.startPlayer()
+    console.log(msg)
+    this.audioRecorderPlayer.addPlayBackListener(e => {
+      if (e.current_position === e.duration) {
+        console.log('finished')
+        this.audioRecorderPlayer.stopPlayer()
+      }
+      this.setState({
+        currentPositionSec: e.current_position,
+        currentDurationSec: e.duration,
+        playTime: this.audioRecorderPlayer.mmssss(
+          Math.floor(e.current_position)
+        ),
+        duration: this.audioRecorderPlayer.mmssss(Math.floor(e.duration))
+      })
+      return
+    })
+  }
+
+  onPausePlay = async () => {
+    await this.audioRecorderPlayer.pausePlayer()
+  }
+
+  onStopPlay = async () => {
+    console.log('onStopPlay')
+    this.audioRecorderPlayer.stopPlayer()
+    this.audioRecorderPlayer.removePlayBackListener()
+    console.log(this.state.durationMillis)
+  }
 
   render() {
     return (
       <View>
-        <Button title="Record" onPress={() => this.onPressRecord()} />
-        <Text>Hello React </Text>
+        <Text>{this.state.durationMillis}</Text>
+        <Button title="Record" onPress={() => this.onStartRecord()} />
+        <Button title="Stop" onPress={() => this.onStopRecord()} />
+        <Button title="Play" onPress={() => this.onStartPlay()} />
+        <Button title="Pause" onPress={() => this.onPausePlay()} />
+        <Button title="Stop" onPress={() => this.onStopPlay()} />
+        <Text style={{ color: 'white', textAlign: 'center' }}>
+          Hello React{' '}
+        </Text>
       </View>
     )
   }
